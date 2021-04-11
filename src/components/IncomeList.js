28e5caee1a328b78b16417ab16145f2011991bb0
堀航大 <kodenginner4521@gmail.com>
2021-04-11 22:01:15 +0900

    少し修正しました

--- src/components/IncomeList.js
+++ src/components/IncomeList.js
@@ -3,6 +3,7 @@ import "../styled.css";
 import { Button } from "./button";
 import firebase from "firebase";
 import { useState } from "react";
+import MediaQuery from "react-responsive";
 
 const GreenLi = styled.li`
   color: rgb(58, 222, 58);
@@ -22,8 +23,18 @@ const FormContainer = styled.form`
   margin-left: 415px;
 `;
 
+const FormContainerResponsive = styled.form`
+  text-align: center;
+  font-size: 10px;
+  margin-left: 205px;
+`;
+
 const SelectContainer = styled.select`
   font-size: 20px;
+`;
+
+const SelectContainerResponsive = styled.select`
+  font-size: 10px;
 `;
 
 const IncomeList = ({ incomes }) => {
@@ -79,34 +90,70 @@ const IncomeList = ({ incomes }) => {
   };
   return (
     <div>
-      <FormContainer>
-        Sort by：
-        <SelectContainer id="sort" name="sort" onChange={() => sortChange()}>
-          <option value="name">名前順</option>
-          <option value="lowPrice">金額の低い順</option>
-          <option value="highPrice">金額の高い順</option>
-          <option value="date">日付順</option>
-        </SelectContainer>
-      </FormContainer>
-      {incomes.map((income) => {
-        const date = new Date(income.date * 1000);
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        return (
-          <ul className="listUl">
-            <DateList className={"listLi"}>
-              {String(month) + "月" + String(day) + "日"}
-            </DateList>
-            <li className="listLi">・{income.label}</li>
-            <GreenLi className="listLi">
-              ¥{income.money.toLocaleString()}円
-            </GreenLi>
-            <SpendingButton onClick={() => incomesDelete(income.id)}>
-              削除
-            </SpendingButton>
-          </ul>
-        );
-      })}
+      <MediaQuery query="(max-width: 767px)">
+        <FormContainerResponsive>
+          Sort by：
+          <SelectContainerResponsive
+            id="sort"
+            name="sort"
+            onChange={() => sortChange()}
+          >
+            <option value="name">名前順</option>
+            <option value="lowPrice">金額の低い順</option>
+            <option value="highPrice">金額の高い順</option>
+            <option value="date">日付順</option>
+          </SelectContainerResponsive>
+        </FormContainerResponsive>
+        {incomes.map((income) => {
+          const date = new Date(income.date * 1000);
+          const month = date.getMonth() + 1;
+          const day = date.getDate();
+          return (
+            <ul className="listUl_responsive">
+              <DateList className={"listLi_responsive"}>
+                {String(month) + "月" + String(day) + "日"}
+              </DateList>
+              <li className="listLi_responsive">・{income.label}</li>
+              <GreenLi className="listLi_responsive">
+                ¥{income.money.toLocaleString()}円
+              </GreenLi>
+              <SpendingButton onClick={() => incomesDelete(income.id)}>
+                削除
+              </SpendingButton>
+            </ul>
+          );
+        })}
+      </MediaQuery>
+      <MediaQuery query="(min-width: 768px)">
+        <FormContainer>
+          Sort by：
+          <SelectContainer id="sort" name="sort" onChange={() => sortChange()}>
+            <option value="name">名前順</option>
+            <option value="lowPrice">金額の低い順</option>
+            <option value="highPrice">金額の高い順</option>
+            <option value="date">日付順</option>
+          </SelectContainer>
+        </FormContainer>
+        {incomes.map((income) => {
+          const date = new Date(income.date * 1000);
+          const month = date.getMonth() + 1;
+          const day = date.getDate();
+          return (
+            <ul className="listUl">
+              <DateList className={"listLi"}>
+                {String(month) + "月" + String(day) + "日"}
+              </DateList>
+              <li className="listLi">・{income.label}</li>
+              <GreenLi className="listLi">
+                ¥{income.money.toLocaleString()}円
+              </GreenLi>
+              <SpendingButton onClick={() => incomesDelete(income.id)}>
+                削除
+              </SpendingButton>
+            </ul>
+          );
+        })}
+      </MediaQuery>
     </div>
   );
 };
